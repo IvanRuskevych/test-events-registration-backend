63c4983643170b51f072b731510d50f905025a79
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-05-20 02:57:21 +0300

    add infinite-scroll

--- controllers/events.js
+++ controllers/events.js
@@ -2,11 +2,22 @@ const {EventModel} = require('../models');
 const {ctrlWrapper, HttpError} = require('../helpers');
 
 const getAllEvents = async (req, res) => {
-    const result = await EventModel.find();
-    if (!result) {
+    const {page = 1, limit = 10} = req.query;
+    const skip = (page - 1) * limit;
+
+    const total = await EventModel.countDocuments();
+    const events = await EventModel.find().skip(skip).limit(parseInt(limit));
+
+    if (!events) {
         throw HttpError(404, 'No Event Found');
     }
 
+    const result={
+        total,
+        events,
+        page: parseInt(page),
+    }
+    console.log("result", result)
     res.json(result);
 };
 
